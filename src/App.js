e59fdcb1d57bfad3a266e30fd10ebf9d8c07c012
unow30 <68832684+unow30@users.noreply.github.com>
2021-04-05 11:19:16 +0900

    react006 life cycle 1. constructor 2. getDerivedStateFromProps 3. render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./styles.css";
-import LifecycleEx from "./R005_LifecycleEx";
+import LifecycleEx from "./R006_LifecycleEx";
 
 export default function App() {
   return (
     <div className="App">
       <h1>start react 200</h1>
       <p>css 적용하기</p>
-      <LifecycleEx />
+      <LifecycleEx prop_value="FromApp.js" />
     </div>
   );
 }
