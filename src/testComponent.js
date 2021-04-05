7aa9e3ab359329ba33b02f43016e1fb51d44f226
unow30 <68832684+unow30@users.noreply.github.com>
2021-04-05 10:30:07 +0900

    Initial commit

---
+++ src/testComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"; // import 로 useState 를 불러온다!
+
+class TestComponent extends Component {
+  render() {
+    return <h2>[this is imported component]</h2>;
+  }
+}
+
+export default TestComponent;
