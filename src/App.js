feafe673872a2b353ae518dd4c01668419b7438f
unow30 <68832684+unow30@users.noreply.github.com>
2021-04-23 11:05:48 +0900

    R034_ReactstrapAlerts R034_ReactstrapAlerts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./styles.css";
-import ReturnMap from "./list/R033_ReturnMap";
+import ReactstrapAlerts from "./reactstrap/R034_ReactstrapAlerts";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>start react 200</h1>
       <p>css 적용하기</p>
-      <ReturnMap />
+      <ReactstrapAlerts />
     </div>
   );
 }
---
+++ src/reactstrap/R034_ReactstrapAlerts.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Alert, UncontrolledAlert } from "reactstrap";
+
+export default class R034_ReactstrapAlerts extends Component {
+  render() {
+    return (
+      <div>
+        <Alert color="light">Simple Alert [color: light]</Alert>
+        <UncontrolledAlert color="warning">
+          Uncontrolled Alert [color: warning]
+        </UncontrolledAlert>
+      </div>
+    );
+  }
+}
