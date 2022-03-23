a8b2d735276d6eb44eb14c519b2d38461f9020ca
unow30 <68832684+unow30@users.noreply.github.com>
2022-03-23 09:43:56 +0900

    restart react restart react repeat life_cycle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./styles.css";
 import ReactstrapButtonDropdown from "./reactstrap/R037_ReactstrapButtonDropdown";
 import "bootstrap/dist/css/bootstrap.css";
-
+import R004 from "./list/R004_LifecycleEx";
 export default function App() {
   return (
-    <div className="App">
+    // <div className="App">
+    //   <h1>start react 200</h1>
+    //   <p>css 적용하기</p>
+    //   <ReactstrapButtonDropdown />
+    // </div>
+    <div>
       <h1>start react 200</h1>
       <p>css 적용하기</p>
-      <ReactstrapButtonDropdown />
+      <R004 prop_value="FromApp.js"></R004>
     </div>
   );
 }
--- src/list/R004_LifecycleEx.js
+++ src/list/R004_LifecycleEx.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react"; // import 로 useState 를 불러온다!
 
 class R004_LifecycleEx extends Component {
+  constructor(props) {
+    console.log("1. constructor call");
+    super(props);
+    this.state = { myvalue: 109 };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("2. getDerivedStateFromProps call");
+    console.log(props);
+    console.log(state);
+    console.log("return changedstate");
+    return { temp_state: props.prop_value };
+  }
+
+  componentDidMount() {
+    console.log("4. componentDidMount call");
+    console.log("5. temp_state is " + this.state.temp_state);
+  }
+
   render() {
     console.log("3. render Call");
     return <h2>[this is Render Function]</h2>;
