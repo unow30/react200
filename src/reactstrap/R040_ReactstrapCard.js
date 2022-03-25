3a751d3b3f924e79332a799ed8082766cea8fe2a
unow30 <68832684+unow30@users.noreply.github.com>
2022-03-25 09:41:59 +0900

    CardImg에러로 다른 예시 적용 CardImg에러로 다른 예시 적용

--- src/reactstrap/R040_ReactstrapCard.js
+++ src/reactstrap/R040_ReactstrapCard.js
@@ -13,20 +13,29 @@ export default class R040_ReactstrapCard extends Component {
   render() {
     return (
       <div>
-        <Card>
-          <CardImg
-            top
-            height="200px"
-            src="https://www.shutterstock.com/image-vector/set-squared-golden-vintage-wooden-frame-1915929097"
-            alt="Card image"
-          >
-            <CardBody>
-              <CardTitle>Card 제목</CardTitle>
-              <CardSubtitle>Card 부제목</CardSubtitle>
-              <CardText>Card 내용 adaddafe</CardText>
-              <Button>버튼</Button>
-            </CardBody>
-          </CardImg>
+        <Card body>
+          <CardTitle tag="h5">Special Title Treatment</CardTitle>
+          <CardText>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </CardText>
+          <Button>Go somewhere</Button>
+        </Card>
+        <Card body className="text-center">
+          <CardTitle tag="h5">Special Title Treatment</CardTitle>
+          <CardText>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </CardText>
+          <Button>Go somewhere</Button>
+        </Card>
+        <Card body className="text-right">
+          <CardTitle tag="h5">Special Title Treatment</CardTitle>
+          <CardText>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </CardText>
+          <Button>Go somewhere</Button>
         </Card>
       </div>
     );
