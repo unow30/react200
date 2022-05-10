92c29a7dad3b92ff7691b2a851734545bbcd5dcf
unow30 <68832684+unow30@users.noreply.github.com>
2022-05-10 09:48:54 +0900

    R044_ReactstrapForm R044_ReactstrapForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactstrapCollapse from "./reactstrap/R042_ReactstrapCollapse";
 import ReactstarpFade from "./reactstrap/R043_ReactstarpFade";
+import ReactstrapForm from "./reactstrap/R044_ReactstrapForm";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -11,6 +12,7 @@ export default function App() {
       <p>css 적용하기</p>
       <ReactstrapCollapse />
       <ReactstarpFade />
+      <ReactstrapForm />
     </div>
   );
 }
