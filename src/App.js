42457438e860e9c5c1479069b6c50d4ec7cbe62a
unow30 <https://github.com/unow30>
2022-05-25 10:13:19 +0900

    R065_Promise
    R065_Promise test
    
    22-05-25 09:16

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import FetchGet from './list/R059_FetchGet';
-import AxiosGet from './list/R601_AxiosGet';
-import Promise from './list/R065_Promise';
+import ReactonClick from './list/R066_onClick';
+import ReactChange from './list/R067_onChange';
 
 import 'bootstrap/dist/css/bootstrap.css';
 export default function App() {
 	return (
 		<div className="App">
 			<h1>start react 200</h1>
-			<FetchGet />
-			<AxiosGet />
-			<Promise/>
+			<ReactonClick/>
+			<ReactChange/>
 		</div>
 	);
 }
---
+++ src/list/R066_onClick.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+class R066_onClick extends Component {
+
+	buttonClick = (param)=>{
+		if(typeof param != 'string') param = "Click a" //param을 받지 않아도 정상적으로 실행할 수 있다.
+		console.log('param :'+param);
+	}
+
+	render(){
+		return (
+			<>
+				<button onClick={e=> this.buttonClick("Click button")}>click button</button>
+				<div onClick={e=> this.buttonClick("Click div")}>Click div</div>
+				<a href="javascript:" onClick={this.buttonClick}>Click a</a>
+			</>
+		)
+	}
+}
+
+export default R066_onClick
