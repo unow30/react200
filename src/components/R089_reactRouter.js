a6433f852e9ce8434b8101b725d7bb74a9027a85
unow30 <https://github.com/unow30>
2022-06-13 09:16:20 +0900

    redux-react-dom Link
    redux-react-dom Link
    
    22-06-13 09:13

--- src/components/R089_reactRouter.js
+++ src/components/R089_reactRouter.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class R089_reactRouter extends Component {
 	render() {
@@ -6,9 +7,15 @@ class R089_reactRouter extends Component {
 			<>
 				<h1>path='/'</h1>
 				<h3>R089_reactRouter</h3>
+				<Link to={'/reactRouter2'}>move to reactRouter2 link</Link>
 			</>
 		)
 	}
 }
 
 export default R089_reactRouter
+
+/*
+* Link는 a태그와 동일하게 작동한다.
+* path를 Link 속성에 연결하면 링크를 클릭할 때 라우팅된 컴포넌트로 이동한다.
+* */
