b7d611a55fbba66fefe495477c1664f78ad44cfc
unow30 <https://github.com/unow30>
2022-06-13 09:13:11 +0900

    redux-react-dom
    redux-react-dom 변경점 확인, 기록
    
    22-06-13 09:13

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Routes,Route} from 'react-router-dom';
+import ReactRouter from './R089_reactRouter'
+import ReactRouter2 from './R089_reactRouter2'
+
+export default class App extends Component {
+	render(){
+		return (
+			<div className={'App'}>
+				<Routes>
+					{/*<Route exact path='/what' component={reactRouter}/>*/}
+					<Route exact path='/' element={<ReactRouter/>}/>
+					{/*<Route exact path='/reactRouter2' component={reactRouter2}/>*/}
+					<Route exact path='/reactRouter2' element={<ReactRouter2/>}/>
+				</Routes>
+			</div>
+		)
+	}
+}
+/*
+* Routes 안에 Route를 입력해야 한다. route 사용법 변경
+* One of the most exciting changes in v6 is the powerful new <Routes> element.
+* This is a pretty significant upgrade from v5's <Switch> element
+* with some important new features including
+* relative routing and linking, automatic route ranking, and nested routes and layouts.
+*
+* Route 태그 안에 경로별 실행할 컴포넌트를 표시할 때 component가 아니라 element로 표시해야 한다.
+* 또한 리엑트 태그 규칙에 따라 태그명은 첫글자 대문자로 적어야 한다.
+* Matched leaf route at location "/reactRouter2" does not have an element.
+* This means it will render an <Outlet /> with a null value by default resulting in an "empty" page.
+* */
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
-import App from "./App";
+import {BrowserRouter} from 'react-router-dom'
+import App from "./components/App";
+// import * as serviceWorker from './serviceWorker'
+// import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
   rootElement
 );
+
+/*
+* ./components/App의 App을 라우팅 전용 컴포넌트로 사용할 것이다.
+* App과 하위 컴포넌트에서 Route, Link를 사용한다.
+* BrowserRouter로 감싸진 하위 컴포넌트에서 Route와 Link를 사용할 수 있다.
+* Route는 호출하는 url에 따라 이동할 component를 정한다.
+* Link는 a태그와 같이 페이지에 표시되는 링크를 클릭하면 url을 호출한다.
+*
+* */
