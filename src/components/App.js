e01cb65110cac764c883ac7f16189eb93e664a00
unow30 <https://github.com/unow30>
2022-06-14 09:07:05 +0900

    redux-react-dom Link
    redux-react-dom Link
    
    22-06-13 09:13

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,24 @@ import {Routes,Route} from 'react-router-dom';
 import ReactRouter from './R089_reactRouter'
 import ReactRouter2 from './R089_reactRouter2'
 
+import '../css/new.css'
+import HeaderAdmin from './Header/Header admin'
+import Footer from './Footer/Footer'
+
 export default class App extends Component {
 	render(){
 		return (
 			<div className={'App'}>
-				<Routes>
 					{/*<Route exact path='/what' component={reactRouter}/>*/}
+				{/*<Route exact path='/reactRouter2' component={reactRouter2}/>*/}
+
+
+				<HeaderAdmin/>
+				<Routes>
 					<Route exact path='/' element={<ReactRouter/>}/>
-					{/*<Route exact path='/reactRouter2' component={reactRouter2}/>*/}
 					<Route exact path='/reactRouter2' element={<ReactRouter2/>}/>
 				</Routes>
+				<Footer/>
 			</div>
 		)
 	}
